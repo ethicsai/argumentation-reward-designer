51363c823b7a317fe0ec4579eec526317ebeefc4
rchaput <rchaput.pro@gmail.com>
2023-05-25 11:26:25 +0200

    Add ReactFlow
    * Installed the ReactFlow library.
    * Replaced the default (placeholder) template for React apps with the quickstart code for ReactFlow (hardcoded nodes and edges).

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+import ReactFlow from 'reactflow';
+
+import 'reactflow/dist/style.css';
+
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: 'First node' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: 'Second node' } },
+];
+
+const initialEdges = [
+  { id: 'e1-2', source: '1', target: '2' }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow nodes={initialNodes} edges={initialEdges} />
     </div>
   );
 }
