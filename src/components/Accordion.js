839398c3b02054ce3ae8805edb24c54c1374c3e7
rchaput <rchaput.pro@gmail.com>
2024-01-09 21:49:48 +0100

    Added a custom Accordion
    MUI already includes an Accordion component, which we can use for
    our side panels; this custom component styles the default one to
    simplify usage, and also handles internally the expanded state.

---
+++ src/components/Accordion.js
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import MuiAccordion from '@mui/material/Accordion';
+import MuiAccordionSummary from '@mui/material/AccordionSummary';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+import { useState } from "react";
+
+export const CustomizedAccordion = styled((props) => {
+  const [expanded, setExpanded] = useState(props?.expanded ?? false);
+  return (
+    <MuiAccordion
+      disableGutters
+      elevation={0}
+      square
+      {...props}
+      expanded={expanded}
+      onChange={(event) => setExpanded(!expanded)}
+    />
+  )
+})(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  '&:not(:last-child)': {
+    borderBottom: 0,
+  },
+  '&::before': {
+    display: 'none',
+  },
+}));
+
+export const CustomizedAccordionSummary = styled((props) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === 'dark'
+      ? 'rgba(255, 255, 255, .05)'
+      : 'rgba(0, 0, 0, .03)',
+  flexDirection: 'row-reverse',
+  '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+    transform: 'rotate(90deg)',
+  },
+  '& .MuiAccordionSummary-content': {
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+export const CustomizedAccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(2),
+  borderTop: '1px solid rgba(0, 0, 0, .125)',
+}));
